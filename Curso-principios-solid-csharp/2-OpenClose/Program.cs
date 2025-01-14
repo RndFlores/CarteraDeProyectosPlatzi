b1c1d555e3e1db37a99d1cb7ee55e4cecaa1978e
ronaldo.fsoto@gmail.com <ronaldo.fsoto@gmail.com>
2025-01-14 12:37:35 -0500

    Arreglando Error de archivos, y principio de OPEN/CLOSED

---
+++ Curso-principios-solid-csharp/2-OpenClose/Program.cs
@@ -0,0 +1,24 @@
+﻿using OpenClose;
+
+ShowSalaryMonthly(new List<Employee>() {
+    new EmployeeFullTime("Pepito Pérez", 160),
+    new EmployeePartTime("Manuel Lopera", 180),
+    new EmployeeContractor("Manuel perez", 200)
+});
+
+
+void ShowSalaryMonthly(List<Employee> employees) 
+{
+    foreach (var employee in employees)
+    {
+     Console.WriteLine($"Empleado: {employee.Fullname}, Pago: {employee.CalculateSalaryMonthly():C1} ");
+    }
+
+}
+
+/*
+ SE CUMPLE EL PRINCIPIO DE RESPONSABILIDAD OPEN/CLOSED
+LOGRANDO QUE LA CLASE PROGRAM.CS EN UN FUTURO SI SE AGREGAN NUEVAS CLASES QUE TENGAN QUE HEREDAR DE EMPLOYEE.CS AL CLASE PROGRAM
+NO SE VERA AFECTADA YA QUE SIGUE EN FUNCIONALIDAD.
+SE CUMPLE EL PRINCIPIO PORQUE EL COMPONTENTE ESTÁ ABIERTO A EXTENCIONES Y CERRADO A MODIFICACIONES.
+ */
