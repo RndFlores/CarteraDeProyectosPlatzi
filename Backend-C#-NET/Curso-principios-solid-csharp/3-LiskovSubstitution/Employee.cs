04548cd368e5996e8919a532676fbe44b3497e5a
ronaldo.fsoto@gmail.com <ronaldo.fsoto@gmail.com>
2025-01-14 17:09:42 -0500

    PRINCIPIOS LISKOV CON CONCLUSION

--- Backend-C#-NET/Curso-principios-solid-csharp/3-LiskovSubstitution/Employee.cs
+++ Backend-C#-NET/Curso-principios-solid-csharp/3-LiskovSubstitution/Employee.cs
@@ -4,18 +4,23 @@ namespace Liskov
     {
         public string Fullname { get; set; }
         public int HoursWorked { get; set; }
-        public int ExtraHours {get;set;}
 
-        public  Employee(string fullname, int hoursWorked, int ExtraHours)
+        //public int ExtraHours {get;set;} NO DEBERÍA IR PORQUE EMPLOYEE CONTRACTOR NO LO USA
+
+        public  Employee(string fullname, int hoursWorked)
         {
             Fullname = fullname;
             HoursWorked = hoursWorked;
-        }  
+        }
 
+        public abstract decimal CalculateSalary();
+
+        /*
+        NO DEBE IR ACA PORQUE LA CLASE PADRE NO ES EL ENCARGADO DE REALIZAR ESTAS ACCIONES 
         public virtual decimal CalculateSalary (bool IsFullTime)
         {   
             decimal hourValue = IsFullTime ? 50 : 40;
             return hourValue * (HoursWorked + ExtraHours);
-        } 
+        } */
     }
 }
