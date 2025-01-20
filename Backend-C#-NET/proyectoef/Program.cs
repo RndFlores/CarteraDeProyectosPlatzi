6964acf6f0f8773956be0159ea1859da4bb3f87d
ronaldo.fsoto@gmail.com <ronaldo.fsoto@gmail.com>
2025-01-19 19:40:56 -0500

    Conexion exitosa a una bd en memoria con la confuguracion que tenemos

--- Backend-C#-NET/proyectoef/Program.cs
+++ Backend-C#-NET/proyectoef/Program.cs
@@ -1,6 +1,23 @@
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using proyectoef;
+
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+
+builder.Services.AddDbContext<TareasContext>(
+    p=>p.UseInMemoryDatabase("TareasDB")// funcion de entity framewoek core
+    );
+
+var app = builder.Build();//Momento donde se construye la aplicacion y escuchando peticiones
 
 app.MapGet("/", () => "Hello World!");
+
+//creo una bd en memoria exitosamente con la configuracion que tenemos, ahora es el momento de conectarse a una bd de verdad
+app.MapGet("/dbconexion", async ([FromServices] TareasContext dbContext) =>
+{
+    dbContext.Database.EnsureCreated();//crea la db
+    return Results.Ok("Base de datos en memoria: " + dbContext.Database.IsInMemory());
+});
 
 app.Run();
--- Backend-C#-NET/proyectoef/obj/Debug/net9.0/proyectoef.AssemblyInfo.cs
+++ Backend-C#-NET/proyectoef/obj/Debug/net9.0/proyectoef.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("proyectoef")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8aba3b78b99a02020e054922f742700b53468bb8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ed957f681b41cf1e2163ccb409b651ef7578e33")]
 [assembly: System.Reflection.AssemblyProductAttribute("proyectoef")]
 [assembly: System.Reflection.AssemblyTitleAttribute("proyectoef")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
