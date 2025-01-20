1ed957f681b41cf1e2163ccb409b651ef7578e33
ronaldo.fsoto@gmail.com <ronaldo.fsoto@gmail.com>
2025-01-19 19:30:34 -0500

    Configuracion ppara el Id y ForeignKey para las tablas

--- Backend-C#-NET/proyectoef/Models/Categoria.cs
+++ Backend-C#-NET/proyectoef/Models/Categoria.cs
@@ -1,7 +1,13 @@
-﻿namespace proyectoef.Models;
+﻿using System.ComponentModel.DataAnnotations; // para la Key
+
+namespace proyectoef.Models;
 public class Categoria
-    {
-    public Guid CategoriaId { get; set; }
+{
+    [Key] //forzamos a que use esto como Id
+    public Guid CategoriaId { get; set; }//Entity Framework le permite automaticamente identificar cual es el id, porq termina en id
+
+    [Required]//campo requerido
+    [MaxLength(255)]//cantidad de caracteres
     public string Nombre { get; set; }
     public string Descripcion { get; set; }
     //traer todas las tareas que estaran asociadas con categorias
--- Backend-C#-NET/proyectoef/Models/Tarea.cs
+++ Backend-C#-NET/proyectoef/Models/Tarea.cs
@@ -1,18 +1,29 @@
 ﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace proyectoef.Models;
 
 public class Tarea
 {
+    [Key]
     public Guid TareaId { get; set; }
+    [ForeignKey("CategoriaId")]
     public Guid CategoriaId { get; set; }
+    [Required]
+    [MaxLength(200)]
     public string Titulo {  get; set; }
+
     public string Descripcion {  get; set; }
     public Prioridad PrioridadTarea { get; set; }
     public DateTime FechaCreacion { get; set; }
 
     // cada tarea tendra asignada una categoria
     public virtual Categoria categoria { get; set; }
+
+    //propiedad que se va a lelnar con Titulo y Descripcion
+    [NotMapped]//En el momento que se haga el mapeo, del contexto a la bd esto sea omitido
+    public string Resumen {  get; set; }
 }
 
 public enum Prioridad
