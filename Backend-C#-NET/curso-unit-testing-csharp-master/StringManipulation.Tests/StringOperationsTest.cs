7ec6d99407ef0a4430acb503bd844b0d2c9f33b1
ronaldo.fsoto@gmail.com <ronaldo.fsoto@gmail.com>
2025-01-26 15:11:41 -0500

    Creacion de los TEST, para IsPalindrome y Concatenate

--- Backend-C#-NET/curso-unit-testing-csharp-master/StringManipulation.Tests/StringOperationsTest.cs
+++ Backend-C#-NET/curso-unit-testing-csharp-master/StringManipulation.Tests/StringOperationsTest.cs
@@ -12,9 +12,33 @@ namespace StringManipulation.Tests
         [Fact]
         public void ConcatenateStrings()
         {
+            //arrange
             var strOperations = new StringOperations();
+            //act
             var result = strOperations.ConcatenateStrings("hello", "world");
+            //assert
+            Assert.NotNull(result);//verificar que no sea nulo.
+            Assert.NotEmpty(result);//verificar que no sea vacío.
             Assert.Equal("hello world", result);//verificar si los resultados son iguales
+        }
+
+        [Fact]
+        public void IsPalindrome_True()
+        {
+            var strOperations = new StringOperations();
+
+            var result = strOperations.IsPalindrome("ala");
+
+            Assert.True(result);
+        }
+        [Fact]
+        public void IsPalindrome_False()
+        {
+            var strOperations = new StringOperations();
+
+            var result = strOperations.IsPalindrome("hola");
+
+            Assert.False(result);
         }
     }
 }
