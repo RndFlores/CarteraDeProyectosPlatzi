b7aad7081b20b5f8e20531d6d8466e3e140e4cd1
ronaldo.fsoto@gmail.com <ronaldo.fsoto@gmail.com>
2025-01-26 15:32:29 -0500

    Manejo de Excepciones y otras opciones con Assert

--- Backend-C#-NET/curso-unit-testing-csharp-master/StringManipulation.Tests/StringOperationsTest.cs
+++ Backend-C#-NET/curso-unit-testing-csharp-master/StringManipulation.Tests/StringOperationsTest.cs
@@ -23,6 +23,34 @@ namespace StringManipulation.Tests
         }
 
         [Fact]
+        public void GetStringLength_Exception()
+        {
+            var strOperations = new StringOperations();
+            Assert.ThrowsAny<ArgumentNullException>(
+                () => //asignamos a un delegado
+                strOperations.GetStringLength(null));
+        }
+        [Fact]
+        public void GetStringLength() { 
+            var strOprations= new StringOperations();
+            var result = strOprations.GetStringLength("Hola");
+
+            Assert.Equal(4, result);
+        }
+
+        [Fact]
+        public void RemoveWhitespace()
+        {
+            var strOperations = new StringOperations();
+            var result =strOperations.RemoveWhitespace("Hello World!!");
+        
+            Assert.NotNull(result);
+            Assert.NotEmpty(result);
+            Assert.Equal("HelloWorld!!", result);
+        }
+
+
+        [Fact]
         public void IsPalindrome_True()
         {
             var strOperations = new StringOperations();
@@ -39,6 +67,20 @@ namespace StringManipulation.Tests
             var result = strOperations.IsPalindrome("hola");
 
             Assert.False(result);
+        }
+
+        [Fact]
+        public void QuantintyInWords()
+        {
+            var strOperations = new StringOperations();
+            var result = strOperations.QuantintyInWords("car", 7);
+
+            Assert.NotNull (result);
+            Assert.NotEmpty(result);
+            Assert.StartsWith("siete", result);//empieze con un texto en particular
+            Assert.EndsWith("cars", result);
+            Assert.Contains("car", result);//que contenga la palabra car
+            Assert.Equal("siete cars", result);
         }
     }
 }
